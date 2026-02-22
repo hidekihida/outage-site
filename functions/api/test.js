b14deba7632714da0de7b73e30c888545050e028
hidekihida <hidekihida@gmail.com>
2026-02-22 13:04:27 -0300

    Update test.js

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,3 +1,11 @@
-export async function onRequest() {
-  return new Response("Functions funcionando 🚀");
+export async function onRequest(context) {
+  const { DB } = context.env;
+
+  const result = await DB
+    .prepare("SELECT name FROM sqlite_master WHERE type='table';")
+    .all();
+
+  return new Response(JSON.stringify(result.results), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
